0b6f6b103c63007e1074e66c156d546eb347f856
404Ako <91970555+404Ako@users.noreply.github.com>
2021-10-06 03:09:45 +0300

    Update lindroid.py

--- lindroid.py
+++ lindroid.py
@@ -3,27 +3,63 @@ from colorama import Fore
 import time
 from pyfiglet import figlet_format
 
+os.system("title Lindroid | | Linux In Android")
 green = Fore.GREEN
 blue = Fore.BLUE
 white = Fore.WHITE
 if os.name == "nt":
-	os.system("cls")
+        os.system("cls")
 else:
-	os.system("clear")
+        os.system("clear")
 def menu():
-		
-	print(Fore.RED+"     Welcome To Android Linux Installer")
-	time.sleep(1)
-	print(Fore.BLUE+(figlet_format("Lindroid", font = "slant")))
-	time.sleep(1)
-	print("Type help to check all installable systems!")
+
+        print(Fore.RED+"     Welcome To Android Linux Installer")
+        time.sleep(1)
+        print(Fore.BLUE+(figlet_format("Lindroid", font = "slant")))
+        time.sleep(1)
+        print("Type help to check all installable systems!")
 def main():
-	time.sleep(1)
-	inp = input(f"""
+        time.sleep(1)
+        inp = input(f"""
 {green}┌──{green}(\{blue}u0_a173㉿localhost{green}){green}-[{white}~{green}]
 └─${white} """)
-	if inp == "help":
- 		print(Fore.BLUE+"""
+        if inp == "help":
+                print(Fore.BLUE+"""
+| ============Lindroid============== |
+| help    -     shows this command   |
+| distros -     shows all systems    |
+| clear   -     clears screen        |
+| credit  -     shows credits        |
+| support -     sends support server |
+| exit    -     exits out of script  |
+| ================================== |""")
+                main()
+        elif inp == "exit":
+                print(Fore.YELLOW+"thanks for using lindroid :)")
+                exit()
+        elif inp == "support":
+                print("https://discord.gg/9tX23eYb")
+                main()
+        elif inp == "clear":
+                if os.name == "nt":
+                        os.system("cls")
+                        main()
+                else:
+                        os.system("clear")
+                        main()
+        elif inp == "credit":
+                print(Fore.BLUE+"""
+| ================================== |
+|          Made By 404Ako            |
+|         ————————————————           |
+| This project was made for the mob- |
+| le coders that can't code due to   |
+| them not having a real linux expe- |
+| rience and for the pentesters etc. |
+| ================================== |""")
+                main()
+        elif inp == "distros":
+                print(Fore.BLUE+"""
 | ====================================== |
 | 1 - Kali Linux           2 - Arch      |
 | 3 - Ubuntu               4 - Debian    |
@@ -31,7 +67,7 @@ def main():
 | 7 - Void                 8 - Alpine    |
 |            9 - Parrot OS               |
 | ====================================== |""")
- 		main()
+                main()
 	elif inp == "1":
 		os.system("pkg update -y && pkg install wget curl proot tar -y && wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/Installer/Kali/kali-xfce.sh -O kali-xfce.sh && chmod +x kali-xfce.sh && bash kali-xfce.sh")
 	elif inp == "2":
